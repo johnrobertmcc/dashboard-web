af1533e6785f1f937fca8de5d25aa0fd2270b654
johnrobertmcc <john.robert.mcc@gmail.com>
2022-06-21 23:16:23 -0400

    added frontend client

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+/**
+ * Dashboard View for J.R. McCann.
+ *
+ * @returns {Element}  The Dashboard Web View and Modifier.
+ */
+function App() {
+  return <div className="underline">this is the app</div>;
+}
+
+export default App;
