876bfed34e4d3550fe4ec18b5fc727242236faca
johnrobertmcc <john.robert.mcc@gmail.com>
2022-08-14 18:16:46 -0400

    8/14/22 - Extracted Heading and components into seperate functions

--- client/src/components/pages/Dashboard/Dashboard.js
+++ client/src/components/pages/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
-import styles from './Dashboard.module.css';
-import { useEffect, useState, useRef } from 'react';
-import { getBudget, publishItem } from 'features/budget/budgetSlice.js';
+import { useEffect } from 'react';
+import { getBudget } from 'features/budget/budgetSlice.js';
 import { useSelector, useDispatch } from 'react-redux';
-import { Input } from 'components/utils';
+import Calendar from 'components/Calendar';
 import Loading from 'components/utils/Loading';
+import AddExpense from 'components/AddExpense';
+import UserExpenses from 'components/UserExpenses';
+import PageHeading from 'components/utils/PageHeading';
 
 /**
  * Renders the default view "/" to display the user's information.
@@ -16,97 +18,19 @@ import Loading from 'components/utils/Loading';
 export default function Dashboard() {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state?.auth);
-  const { items } = useSelector((state) => {
-    return state.budget;
-  });
-  const defaultState = {
-    amount: 0,
-    event: '',
-    tag: '',
-    item: '',
-    date: new Date().toLocaleDateString(),
-  };
-  const [data, setData] = useState(defaultState);
 
   useEffect(() => {
     dispatch(getBudget());
   }, [user]);
 
-  /**
-   * Function used to update the input with user information.
-   *
-   * @author John Robert McCann
-   * @since 6/26/2022
-   * @param {Event} e  The user event.
-   */
-  function handleChange(e) {
-    e.preventDefault();
-    setData((prev) => ({
-      ...prev,
-      [e.target.name]: e?.target?.value,
-    }));
-  }
-
-  /**
-   * Function used submit a budget item to the database.
-   *
-   * @author John Robert McCann
-   * @since 6/27/2022
-   * @param {Event} e  The user event.
-   */
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setData(defaultState);
-    dispatch(publishItem(data));
-  }
   return (
     <section>
-      {user && items && items?.budget ? (
+      {user ? (
         <>
-          <h1>Welcome {user && user?.name}</h1>
-          <h2 className={styles.subHead}>Current Expenses</h2>
-          <table className={styles.table}>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Item</th>
-                <th>Amount</th>
-                <th>Event</th>
-                <th>Tag</th>
-              </tr>
-            </thead>
-            <tbody>
-              {items.budget.map((item, i) => {
-                return (
-                  <tr key={i}>
-                    <td>{new Date(item?.date).toLocaleDateString()}</td>
-                    <td>{item?.item || '-'}</td>
-                    <td>{item?.amount?.$numberDecimal || '-'}</td>
-                    <td>{item?.event || '-'}</td>
-                    <td>{item?.tag || '-'}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-          <h2 className={styles.subHead}>Add Expense</h2>
-          <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
-            {Object.keys(data).map((input, i) => {
-              return (
-                <Input
-                  key={i}
-                  id={input}
-                  name={input}
-                  value={data[input]}
-                  placeHolder={input}
-                  callBack={(e) => handleChange(e)}
-                />
-              );
-            })}
-            <button type="submit" className={styles.submit}>
-              Submit
-            </button>
-          </form>
+          <PageHeading message={`Welcome ${user?.name}`} />
+          <UserExpenses />
+          <AddExpense />
+          <Calendar />
         </>
       ) : (
         <Loading />
