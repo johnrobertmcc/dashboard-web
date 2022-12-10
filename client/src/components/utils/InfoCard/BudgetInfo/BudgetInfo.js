a55fe94f8fb5ca4e5004f248954e188f3f20647e
johnrobertmcc <john.robert.mcc@gmail.com>
2022-12-10 00:46:46 -0500

    Styling

--- client/src/components/utils/InfoCard/BudgetInfo/BudgetInfo.js
+++ client/src/components/utils/InfoCard/BudgetInfo/BudgetInfo.js
@@ -2,6 +2,7 @@ import { useCalendarContext } from 'context/CalendarData/CalendarData';
 import Loading from 'components/utils/Loading';
 import InfoCard from 'components/utils/InfoCard';
 import delayedRender from 'functions/utils/delayedRender';
+import styles from './BudgetInfo.module.scss';
 
 /**
  * Renders a card for budget information.
@@ -9,10 +10,11 @@ import delayedRender from 'functions/utils/delayedRender';
  * @author  John Robert McCann
  * @since   09/18/2022
  * @version 1.0.0
- * @return  {Element}    The BUdgetInfo component.
+ * @return  {Element}    The BudgetInfo component.
  */
 export default function BUdgetInfo() {
   const { monthTotal } = useCalendarContext();
+  const { total = 'LOADING', items = 'LOADING' } = monthTotal;
 
   if (!monthTotal) {
     return <Loading />;
@@ -20,8 +22,14 @@ export default function BUdgetInfo() {
 
   return (
     <InfoCard>
-      <h3>Total Spent This Month: {delayedRender(monthTotal?.total)}</h3>
-      <h3>Total Transactions: {delayedRender(monthTotal?.items)}</h3>
+      <div className={styles.userInfoWrapper}>
+        <h3 className={styles.total}>
+          Total Spent This Month: {delayedRender(total)}
+        </h3>
+        <h3 className={styles.transactions}>
+          Total Transactions: {delayedRender(items)}
+        </h3>
+      </div>
     </InfoCard>
   );
 }
